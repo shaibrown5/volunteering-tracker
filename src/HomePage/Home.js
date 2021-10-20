07abe1303be4533ecf2b9eb379f170d0ff99f6b8
shai <shaibrwon95@gmail.com>
2021-10-20 22:06:52 +0300

    removed sass

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -1,28 +1,37 @@
 import React, {useState} from 'react';
-import {Text, TextInput, View} from 'react-native';
-import styles from './home.scss';
+import {Text, TextInput, View, StyleSheet} from 'react-native';
 
 const Home = () => {
   const [hours, setHours] = useState(-1);
   const [place, setPlace] = useState('');
 
   return (
-    <View style={{padding: 10}}>
-      <Text>place hand here </Text>
+    <View style={styles.homeContainer}>
+      <Text>
+        <Text style={styles.homeText}>Place hand here</Text>
+      </Text>
       <TextInput
-        style={styles.home}
+        style={styles.inputText}
         placeholder="Place of volenteer"
         onChangeText={text => setPlace(text)}
         defaultValue={place}
       />
-      <Text style={{padding: 10, fontSize: 42}}>
-        {place
-          .split(' ')
-          .map(word => word && '🍕')
-          .join(' ')}
-      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  homeContainer: {
+    padding: 20,
+  },
+  homeText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'purple',
+  },
+  inputText: {
+    fontSize: 20,
+  },
+});
 
 export default Home;
